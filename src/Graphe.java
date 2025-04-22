641537e9ec793deb0a31929ab308afcc321d4655
ouail <ouailzouina01@gmail.com>
2025-04-22 15:06:30 +0200

    correction de Djikstra et l'ajout de FLOYD_WARSHALL et comparaison est ce qu'ils ont le même résultat

--- src/Graphe.java
+++ src/Graphe.java
@@ -1,5 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Graphe {
     record arret(int distination,int cout){}
@@ -12,37 +16,53 @@ public class Graphe {
 
 
 
-    public Graphe(){
-        nbSommets=4;
-        tailleListPriorite=nbSommets-1;
-        listPriorite = new int[tailleListPriorite];
-        d = new int[nbSommets];
-        pred = new int[nbSommets];
-        graphe=new ArrayList<>();
-        for(int i=0;i<nbSommets;i++){
-            graphe.add(new ArrayList<>());
-            d[i]=-1;
-            pred[i]=-1;
-            listPriorite[i]=i;
+    public Graphe(String nomFichier){
+        try{
+            File file = new File("src/" + nomFichier);
+            Scanner scanner = new Scanner(file);
+            nbSommets = scanner.nextInt();
+            tailleListPriorite = nbSommets;
+            listPriorite = new int[tailleListPriorite];
+            d = new int[nbSommets];
+            pred = new int[nbSommets];
+            graphe=new ArrayList<>();
+
+            //initialisation des list des arret de graphe
+            for(int i=0; i<nbSommets; i++){
+                graphe.add(new ArrayList<>());
+                d[i]=-1;
+                pred[i]=-1;
+                listPriorite[i]=i;
+            }
+            tailleListPriorite--  ;
+            d[0]=0;
+            while (scanner.hasNextInt()) {
+                int sommet = scanner.nextInt();
+                int dest = scanner.nextInt();
+                int cout = scanner.nextInt();
+                graphe.get(sommet).add(new arret(dest, cout));
+            }
+
+
+            scanner.close();
+
+        } catch(FileNotFoundException e){
+            System.out.println("erreur : " + e.getMessage());
         }
-        d[0]=0;
-        graphe.get(0).add(new arret(1,3));
-        graphe.get(0).add(new arret(2,9));
-        graphe.get(1).add(new arret(2,6));
-        graphe.get(1).add(new arret(3,2));
-        graphe.get(2).add(new arret(3,8));
     }
 
     // renvoyer le sommet le plus priorité
     public int getSommetPlusPriorite(){
         int min = Integer.MAX_VALUE;
-        for(int i=0;i<tailleListPriorite;i++){
+        int sommet = -1 ;
+        for(int i=0; i<tailleListPriorite; i++){
             int coutSommet = d[listPriorite[i]];
             if ( coutSommet < min && coutSommet != -1 ){
                 min=d[listPriorite[i]];
+                sommet = listPriorite[i] ;
             }
         }
-        return min;
+        return sommet;
     }
 
     public int getIndice(int sommet){
@@ -54,23 +74,103 @@ public class Graphe {
         return -1;
     }
 
+    public int[] getPred() {
+        return pred;
+    }
+    public  int[] getD(){ return d; }
 
     public void dijkstra(){
-        while (tailleListPriorite >= 0){
+        while (tailleListPriorite > 0){
+
             int sommetPlusPriorite = getSommetPlusPriorite();
+            if (sommetPlusPriorite == -1) break;
             graphe.get(sommetPlusPriorite).forEach(arret -> {
-                if(d[arret.distination] == -1) d[arret.distination]++ ;
-                d[arret.distination] += arret.cout;
-                pred[arret.distination]= sommetPlusPriorite;
+                if (d[arret.distination] == -1 || d[arret.distination] > d[sommetPlusPriorite] + arret.cout) {
+                    d[arret.distination] = d[sommetPlusPriorite] + arret.cout;
+                    pred[arret.distination] = sommetPlusPriorite;
+                }
             });
+
+
             int indice = getIndice(sommetPlusPriorite) ;
-            if(indice == tailleListPriorite){
+            if(indice == tailleListPriorite-1){
                 tailleListPriorite-- ;
             } else{
-                listPriorite[indice] = listPriorite[tailleListPriorite] ;
+                listPriorite[indice] = listPriorite[tailleListPriorite-1] ;
                 tailleListPriorite--;
             }
         }
     }
+
+
+
+    //-------------------------------------------------------------------------------------------------
+    // plus court chemin FLOYD-WARSHALL
+    private int[][] distance ; // distance minimale entre chaque paire
+    public Graphe(String nomFichier , String type) throws FileNotFoundException { // le deuxième argument seulement pour distinguer les deux constructeur
+        int INF = Integer.MAX_VALUE ;
+        File file = new File("src/" + nomFichier);
+        Scanner scanner = new Scanner(file);
+        nbSommets = scanner.nextInt();
+        this.distance = new int[nbSommets][nbSommets];
+
+        // Initialiser la matrice
+        for (int i = 0; i < nbSommets; i++) {
+            for (int j = 0; j < nbSommets; j++) {
+                if (i == j) distance[i][j] = 0;
+                else distance[i][j] = INF;
+            }
+        }
+
+        // Charger les arcs depuis le fichier
+        while (scanner.hasNextInt()) {
+            int u = scanner.nextInt();
+            int v = scanner.nextInt();
+            int w = scanner.nextInt();
+            distance[u][v] = w;
+        }
+    }
+
+    public void FLOYD_WARSHALL(){
+        int INF = Integer.MAX_VALUE ;
+        for (int k = 0; k < nbSommets; k++) {
+            for (int i = 0; i < nbSommets; i++) {
+                for (int j = 0; j < nbSommets; j++) {
+                    if (distance[i][k] != INF && distance[k][j] != INF) {
+                        distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
+                    }
+                }
+            }
+        }
+    }
+
+
+    // Méthode affiche la matrice de l'algo FLOYD-WARSHALL
+    public void afficherMatrice() {
+        int n = distance.length;
+        final int INF = Integer.MAX_VALUE;
+
+        // Affichage en forme de tableau avec en-têtes
+        System.out.print("     ");
+        for (int j = 0; j < n; j++) {
+            System.out.printf("%5d", j);
+        }
+        System.out.println();
+        System.out.println("    " + "-".repeat(5 * n));
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%3d |", i);
+            for (int j = 0; j < n; j++) {
+                if (distance[i][j] == INF) {
+                    System.out.print("  INF");
+                } else {
+                    System.out.printf("%5d", distance[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        Graphe graphe = new Graphe();
-        System.out.println(graphe.getSommetPlusPriorite());
+    public static void main(String[] args) throws FileNotFoundException {
+        Graphe graphe = new Graphe("graphe.txt", "");
+        graphe.FLOYD_WARSHALL();
+        graphe.afficherMatrice();
+
+
+        Graphe graphe2 = new Graphe("graphe.txt");
+        graphe2.dijkstra();
+        System.out.println(Arrays.toString(graphe2.getD()));
     }
 }
